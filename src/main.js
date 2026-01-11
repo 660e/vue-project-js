7bd6b2aaa296e9595e63453ac87918ca45086cba
Min <solunar@vip.qq.com>
2026-01-11 18:30:04 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import './assets/main.css';
+import './styles/index.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
 import App from './App.vue';
 import router from './router';
 
@@ -10,5 +9,4 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
 app.mount('#app');
